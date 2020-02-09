6b5ca1b2022133c47bb104f0faea401b016a57eb
WongyuChoi <wchoi@ahrinet.org>
2020-02-08 21:31:33 -0500

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  // the following will not work on the production phase
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
